35418e4d1946453776caaee9f08c288d17ffe45f
Maxwell Riesel <maxwellriesel@yahoo.com>
2023-11-06 18:31:17 -0500

    fix carousel image fluidity

--- src/components/ExampleCarouselImage.jsx
+++ src/components/ExampleCarouselImage.jsx
@@ -3,6 +3,7 @@ import React from "react";
 function ExampleCarouselImage(props) {
   return (
     <img
+    className="img-fluid"
       src={props.image}
       alt="Carousel"
       loading="lazy"

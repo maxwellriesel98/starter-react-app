4ffa539a909c080645a452dc8aecf59268e9c848
Maxwell Riesel <maxwellriesel@yahoo.com>
2023-11-06 16:43:21 -0500

    Update Carousel.jsx

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -18,26 +18,15 @@ function MyCarousel(props) {
     >
       <Carousel.Item>
         <ExampleCarouselImage image={props.image1} text="First slide" />
-        <Carousel.Caption>
-          <h3>{props.FirstSlideLabel}</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
+        
       </Carousel.Item>
       <Carousel.Item>
         <ExampleCarouselImage image={props.image2} text="Second slide" />
-        <Carousel.Caption>
-          <h3>{props.SecondSlideLabel}</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
+       
       </Carousel.Item>
       <Carousel.Item>
         <ExampleCarouselImage image={props.image3} text="Third slide" />
-        <Carousel.Caption>
-          <h3>{props.ThirdSlideLabel}</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
+      
       </Carousel.Item>
     </Carousel>
   );

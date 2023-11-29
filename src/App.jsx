3bc08d4c9f65907408343b094c5019cad62a6b2c
Maxwell Riesel <maxwellriesel@yahoo.com>
2023-11-29 16:05:58 -0500

    add components, add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bodycolor">
+      <Header />
+      <Routes>
+        <Route path="/" element=<h1> Home</h1> />
+        <Route path="/about" element=<h1> About</h1> />
+        <Route path="/industrylinks" element=<h1>industry Links</h1> />
+        <Route path="/services" element=<h1> Services</h1> />
+        <Route path="/contact" element=<h1>contact</h1> />
+        <Route path="/success" element=<h1>success</h1>/>
+        <Route path="/failure" element=<h1>failure</h1>/>
+      </Routes>
+      <Footer />
     </div>
   );
 }
